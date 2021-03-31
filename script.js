67394a780444fb5e6ae7b5749e3cef18504c3e67
Krister Viirsaar <git@krister.ee>
2021-03-31 20:44:25 +0300

    Sisesta tellimus veebilehele

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var tellimuseVorm = document.querySelector('form')
+var nimekirjaKoht = document.querySelector('ul')
 
 tellimuseVorm.onsubmit = function(event) {
     event.preventDefault()
     var probleem = tellimuseVorm.description.value
     var aadress = tellimuseVorm.address.value
     var telefon = tellimuseVorm.phone.value
-    console.log(probleem, aadress, telefon)
+
+    var rida = document.createElement('li')
+    rida.innerHTML = probleem + ' / ' + aadress + ' / ' + telefon
+
+    nimekirjaKoht.appendChild(rida)
 }
